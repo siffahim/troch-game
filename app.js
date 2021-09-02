89097df28ba90cf74da51846b127c6b181405cb0
siffahim <siffahim25@gmail.com>
2021-09-02 22:24:45 +0600

    add new site

---
+++ app.js
@@ -0,0 +1,23 @@
+const playerOne = document.getElementById('playOne');
+const playerTwo = document.getElementById('playTwo');
+const message = document.getElementById('header');
+const luduGame = () => {
+    const play1 = Math.floor(Math.random() * 6) + 1;
+    const imgOne = `img/dice${play1}.png`;
+    playerOne.setAttribute('src', imgOne);
+
+
+    const play2 = Math.floor(Math.random() * 6) + 1;
+    const imgTwo = `img/dice${play2}.png`;
+    playerTwo.setAttribute('src', imgTwo);
+
+    if (play1 > play2) {
+        message.innerText = 'Player1 Win!'
+    }
+    else if (play1 < play2) {
+        message.innerText = 'Player2 Win!'
+    }
+    else {
+        message.innerText = 'Draw!'
+    }
+}
