9f361bbf5d66800f02a0caaef228bb1c3f975e80
siffahim <siffahim25@gmail.com>
2021-09-03 19:32:26 +0600

    add style

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 const playerOne = document.getElementById('playOne');
 const playerTwo = document.getElementById('playTwo');
 const message = document.getElementById('header');
+
+document.onkeydown = function(event){
+    if (event.keycode == 123) {
+        return false;
+    }
+    if (event.ctrlKey && event.shiftKey && event.keycode == 'I'.charCodeAt(0)) {
+        return false;
+    }
+    if (event.ctrlKey && event.shiftKey && event.keycode == 'J'.charCodeAt(0)) {
+        return false;
+    }
+    if (event.ctrlKey && event.keycode == 'U'.charCodeAt(0)) {
+        return false;
+    }
+}
+
 const luduGame = () => {
     const play1 = Math.floor(Math.random() * 6) + 1;
     const imgOne = `img/dice${play1}.png`;
@@ -11,6 +27,7 @@ const luduGame = () => {
     const imgTwo = `img/dice${play2}.png`;
     playerTwo.setAttribute('src', imgTwo);
 
+    
     if (play1 > play2) {
         message.innerText = 'Player1 Win!'
     }
